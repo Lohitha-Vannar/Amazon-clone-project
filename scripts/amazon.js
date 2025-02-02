b7097a42d85463fc9b7506ce2a599fd4ffb1e831
Lohitha-Vannar <Lohithavannar@gmail.com>
2025-02-02 19:00:15 +0530

    amazon project

---
+++ scripts/amazon.js
@@ -0,0 +1,87 @@
+//importing the module
+import {cart, addToCart} from '../products-data/cart.js';
+import {products} from '../products-data/products.js';
+
+let productsHtml = '';
+products.forEach((product) => {
+  productsHtml += `
+        <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+           ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${(product.priceCents / 100).toFixed(2)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button  class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+            Add to Cart
+          </button>
+        </div>`;
+});
+
+
+
+document.querySelector('.js-products-grid').innerHTML = productsHtml;
+
+
+function updateCaetQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+    //console.log(cartQuantity);
+  });
+
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  //console.log(cart); 
+
+}
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((buttonElement) => {
+     buttonElement.addEventListener('click', () => {
+      //console.log('Added Product');
+      // the .dataset will gives all informmation or the data attached to that element
+      const productId = buttonElement.dataset.productId;// data-product-name propert is converted into camelCase from kabab-case which is product 
+      
+      addToCart(productId);
+
+      updateCartQuantiyt('')
+    });
+   });
